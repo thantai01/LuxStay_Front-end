ef98d3e9752ecb2ac0f4227f6ff0ca083786f96f
thantai01 <thantaihp2011@gmail.com>
2021-09-09 02:53:35 +0700

    Hùng update view-detail UI

--- src/app/page/view-detail/view-detail.component.ts
+++ src/app/page/view-detail/view-detail.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+import {Apartment} from '../../model/apartment';
 
 @Component({
   selector: 'app-view-detail',
   templateUrl: './view-detail.component.html',
-  styleUrls: ['./view-detail.component.css']
+  styleUrls: ['./view-detail.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class ViewDetailComponent implements OnInit {
-
-  constructor() { }
+  images = [700, 533, 807, 124].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  selectedApartment: Apartment;
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 10000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }
 
   ngOnInit(): void {
   }
