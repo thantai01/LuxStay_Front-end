861c774f637e5bb7479d17cbdc86bb5cfeaaf946
thantai01 <thantaihp2011@gmail.com>
2021-09-13 15:03:33 +0700

    hung update UI

---
+++ src/app/model/notice.ts
@@ -0,0 +1,5 @@
+export interface Notice {
+  id?: number;
+  content?: string;
+  user?: any;
+}
--- src/app/service/order.service.ts
+++ src/app/service/order.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Order} from '../model/order';
 import {Observable} from 'rxjs';
 import {environment} from '../../environments/environment';
+import {Apartment} from '../model/apartment';
 
 const ORDER_API = environment.API_SERVER + '/orders';
 
@@ -15,13 +16,28 @@ export class OrderService {
   booking(order: Order): Observable<any> {
     return this.http.post(ORDER_API, order);
   }
-  findAllOrderByUserId(userId: number): Observable<Order[]> {
-    return this.http.get<Order[]>(ORDER_API + '/' + userId);
-  }
+  // findAllOrderByUserId(userId: number): Observable<Order[]> {
+  //   return this.http.get<Order[]>(ORDER_API + '/' + userId);
+  // }
   findAllOrderOfApartment(apartmentId: number): Observable<Order[]> {
     return this.http.get<Order[]>(ORDER_API + '/findByApartment/' + apartmentId);
   }
   findAllOrderOfUser(userId: number): Observable<Order[]> {
     return this.http.get<Order[]>(ORDER_API + '/findByUser/' + userId);
+  }
+  findOrderById(orderId: number): Observable<Order> {
+    return this.http.get<Order>(ORDER_API + '/' + orderId);
+  }
+  edit(id: number, order: Order): Observable<Apartment> {
+    return this.http.put<Order>(`${ORDER_API}/${id}`, order);
+  }
+  findPendingOrder(apartmentId: number): Observable<Order[]> {
+    return this.http.get<Order[]>(ORDER_API + '/findOrderPending/' + apartmentId);
+  }
+  findPendingOrderOfUser(userId: number): Observable<Order[]> {
+    return this.http.get<Order[]>(ORDER_API + '/findPendingOfUser/' + userId);
+  }
+  deletePendingOrder(orderId: number): Observable<Order> {
+    return this.http.delete(ORDER_API + '/' + orderId);
   }
 }
